198890076ee2eab48ce658720fc277c93fd3c66c
Priyanka Salunke <salunkeps99@gmail.com>
2022-10-28 10:34:27 +0530

    Added struct basics

--- programs/structs.go
+++ programs/structs.go
@@ -42,5 +42,38 @@ func main() {
 		},
 	}
 
-	fmt.Println(person1)
+	person1.print()
+
+	// Give us the access to memory address where the struct person1 is sitting at.
+	// Here person1Pointer is not directly refer to the struct (person1)
+	// Instead, it's a reference to the memory address that struct exists at.
+	person1Pointer := &person1
+	person1Pointer.updateName("Priyanka")
+
+	// second way to call the updateName function:
+	// person1.updateName("Priyanka")
+	// No need to create person1Pointer. Golang will automatically convert and allow us to call the function
+	// and treat as pass by reference
+
+	person1.print()
+}
+
+// receiver function
+func (p person) print() {
+	fmt.Println(p)
+}
+
+// receiver function : update the property (firstname) of the struct
+// Note : This will not update the actual value of the firstname, you can still see "Piyu" as firstname
+// This is due to pass by value mechanism used by Golang
+// func (p person) updateName(name string) {
+// 	p.firstName = name
+// }
+
+// Pointers : updatename receiver function with pointer
+// pass by reference
+// *person receiver is the type which we are interested
+func (pointerToPerson *person) updateName(name string) {
+	// *pointerToPerson (operator)- take the memory address and give us the value that exists at that memory address.
+	(*pointerToPerson).firstName = name
 }
