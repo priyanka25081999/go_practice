332933863f11763c9eb21862d2d0450f76a22c00
Priyanka Salunke <salunkeps99@gmail.com>
2022-11-05 17:07:52 +0530

    Updated cmd.go fle

--- journal_app/cmd/cmd.go
+++ journal_app/cmd/cmd.go
@@ -16,29 +16,11 @@ var cmd = &cobra.Command{
 		// check if the user is authorized or not
 		fmt.Println(entry, " ", username, " ", password)
 
-		// fmt.Println("Authorization check!")
-		// flag = app.ValidateUser(username)
-
-		// if flag {
-		//app.Register()
-
-		// instead of add new to map, we should validate the user
-		//app.AddNewUserToMap(username)
-		//app.DecryptFile()
 		app.GetDataFromFile()
-		//isValid := app.ValidateUser(username)
-		//if isValid {
 		fmt.Println("Valid user!!!")
 		app.AddNewEntry(username, entry)
 		app.AddNewEntryToFile(username)
 		app.EncryptFile()
-		//} else {
-		//fmt.Println("Invalid user!!!")
-		//}
-
-		//app.EntryMenu(username)
-		//} else {
-		//}
 	},
 }
 
@@ -50,7 +32,6 @@ func init() {
 	cmd.PersistentFlags().StringVarP(&entry, "add", "a", "", "add a new entry")
 	cmd.PersistentFlags().StringVarP(&username, "uname", "u", "", "enter username")
 	cmd.PersistentFlags().StringVarP(&password, "passwd", "p", "", "enter password")
-	//cmd.PersistentFlags().StringVarP(&email, "email", "p", "", "enter email")
 
 	// required flags
 	cmd.MarkPersistentFlagRequired("username")
