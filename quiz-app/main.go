dafeb337c1862c021c7a70b1903aa53bccf018ba
Priyanka Salunke <priyanka.salunke@velotio.com>
2022-12-28 09:30:34 +0530

    Added go routines and channels

--- quiz-app/main.go
+++ quiz-app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"os"
@@ -15,7 +16,20 @@ type problem struct {
 // This pulls the problem from quiz.csv file
 // and return the slice of string which contains the que and ans data
 // otherwise it returns an error
-func problemPuller(fileName string) ([]string, error) {
+func problemPuller(fileName string) ([]problem, error) {
+	// open the file
+	fileObj, err := os.Open(fileName)
+	if err == nil {
+		// no error, so read the file
+		csvR := csv.NewReader(fileObj)
+		if line, err := csvR.ReadAll(); err == nil {
+			return parseProblem(line), nil
+		} else {
+			return nil, fmt.Errorf("error in reading csv file")
+		}
+	} else {
+		return nil, fmt.Errorf("error in opening the file %s", fileName)
+	}
 
 }
 
@@ -23,7 +37,12 @@ func problemPuller(fileName string) ([]string, error) {
 // and divides it into problem slice such that it can contains the actual que and ans format
 // It takes lines which are read by problem puller one by one
 func parseProblem(lines [][]string) []problem {
+	r := make([]problem, len(lines))
+	for i := 0; i < len(lines); i += 1 {
+		r[i] = problem{question: lines[i][0], answer: lines[i][1]}
+	}
 
+	return r
 }
 
 func main() {
@@ -43,7 +62,44 @@ func main() {
 	}
 
 	// initialize the correct ans and timer variable
-	correctAnd := 0
+	correctAns := 0
 	timerObj := time.NewTimer(time.Duration(*timer) * time.Second)
 
+	// start looping through the problems
+	// make a channel
+	ansChannel := make(chan string)
+problemLoop:
+	for i, p := range prob {
+		var ans string
+		fmt.Printf("Problem %d: %s = ", i+1, p.question)
+
+		// get the answer and compare it with actual answer
+		// let's use goroutine now
+		go func() {
+			fmt.Scanf("%s", &ans) // get the ans
+			ansChannel <- ans
+		}()
+		select {
+		// there are 2 cases here
+		// 1st, user completed all the quiz before timer ends
+		// 2nd, timer finishes/runs out
+		// in both the cases, we want to end our quiz and show the correct answer count to the user
+		case <-timerObj.C:
+			// this is 2nd case, timer has run out, so we are breaking
+			fmt.Println("\nTimer ends!")
+			break problemLoop
+		case iAns := <-ansChannel:
+			if iAns == p.answer {
+				correctAns++
+			}
+			if i == len(prob)-1 {
+				close(ansChannel)
+			}
+		}
+	}
+
+	// calculate the result and return it to the user
+	fmt.Printf("Your result is %d out of %d\n", correctAns, len(prob))
+	fmt.Printf("Press enter to exit")
+	<-ansChannel
 }
